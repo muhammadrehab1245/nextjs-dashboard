fcbc32aa359a99036a138561f5f83beb6f14ba8c
Muhammad Rehab <rehab123335@gmail.com>
2024-01-05 11:05:21 +0500

    'next continue'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -28,6 +29,18 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
+<div className={styles.shape}></div>;
+<Image   width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version" src="/hero-desktop.png"/>
+  <Image   width={560 }
+        height={620 }
+        className="block md:hidden"
+        alt="Screenshots of the dashboard project showing desktop version" src="/hero-desktop.png"/>
     </main>
   );
 }
